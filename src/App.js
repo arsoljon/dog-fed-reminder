4a6fa5955b79c4f0d30760156822d3f7d8fa0583
jonathan arsola <jonathan.arsola01@utrgv.edu>
2022-05-15 15:22:52 -0500

    strutcture of different pages.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import Error from './pages/Error'
+import History from './pages/History'
+import Reminder from './pages/Reminder'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/reminder">
+            <Reminder />
+          </Route>
+          <Route exact path="/history">
+            <History />
+          </Route>
+          <Route exact path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/History.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const History = () => {
+  return <h1>History component</h1>
+}
+
+export default History
